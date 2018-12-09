f3769ca6b0f7c6b8dd64f88ae7a91d1504203978
Fernando de Jesús Soto <zomaotoko@gmail.com>
2018-12-09 06:02:05 -0600

    Solve Day 1: Part II

--- day 1/answer.cpp
+++ day 1/answer.cpp
@@ -1,11 +1,35 @@
-#include <cstdio>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long getFirstRepeatedFreq(const vector<long> &freqs) {
+    unordered_map<long, int> count;
+    int i = -1;
+    long current_freq = 0;
+    bool found = false;
+    long twice;
+    while (!found) {
+        count[current_freq] ++;
+        if (count[current_freq] == 2) {
+            twice = current_freq;
+            found = true;
+        }
+        i = (i + 1) % freqs.size();
+        current_freq += freqs[i];
+    }
+    return twice;
+}
 
 int main(int argc, char const *argv[]) {
     long ans = 0;
     long n = 0;
+    vector<long> freqs;
     while (scanf("%ld", &n) != EOF) {
+        freqs.push_back(n);
         ans += n;
     }
-    printf("%ld\n", ans);
+
+    long twice = getFirstRepeatedFreq(freqs);
+    printf("%ld %ld\n", twice, ans);
     return 0;
 }
