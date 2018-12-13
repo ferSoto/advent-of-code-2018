694e55e1503ff1af7461df4204540f09edda0529
Fernando de Jesús Soto <zomaotoko@gmail.com>
2018-12-12 22:39:01 -0600

    Solve day 2: Part II

--- day 2/answer.cpp
+++ day 2/answer.cpp
@@ -18,19 +18,55 @@ bool appearsThree(const unordered_map<char, int> map) {
     } return false;
 }
 
+bool differByJustOne(const string &s1, const string &s2) {
+    int errors = 0;
+    for (unsigned int i = 0; i < s1.size(); i++) {
+        if (s1[i] != s2[i]) {
+            errors ++;
+            if (errors > 1) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+string commonChars(const string &s1, const string &s2) {
+    string common = "";
+    for (unsigned int i = 0; i < s1.size(); i++) {
+        if (s1[i] != s2[i]) {
+            continue;
+        }
+        common += s1[i];
+    }
+    return common;
+}
+
 int main() {
     int twice = 0;
     int three = 0;
+    vector<string> ids;
     while (!cin.eof()) {
         string s;
         unordered_map<char, int> count;
         cin >> s;
+        ids.push_back(s);
         for (char c : s) {
             count[c] ++;
         }
         twice += appearsTwice(count);
-        three += appearsThree(count); 
+        three += appearsThree(count);
     }
     cout << twice * three << endl;
+
+    for (unsigned int i = 0; i < ids.size() - 1; i++) {
+        for (unsigned int j = i + 1; j < ids.size(); j++) {
+            if(differByJustOne(ids[i], ids[j])) {
+                cout << commonChars(ids[i], ids[j]) << endl;
+                break;
+            }
+        }
+    }
+
     return 0;
 }
